ee510cf43a0a59d73dae08d1371c4a41ed84a647
Torre <torre.locascio@gmail.com>
2018-07-10 23:20:11 -0400

    finished second tutorial video, fbbutton should be working but getting error, react-native-button cannot be found in haste module map

--- App.js
+++ App.js
@@ -7,15 +7,68 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, Text, View} from 'react-native';
-import firebase from 'react-native-firebase'
+import {FlatList, StyleSheet, Platform, Text, 
+  View, Image, Alert, 
+  TouchableHighlight, 
+  Dimensions, TextInput} from 'react-native';
+import firebase from 'react-native-firebase';
+import {AccessToken, LoginManager, LoginButton} from 'react-native-fbsdk';
+import Button from 'react-native'
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props){
+    super(props);
+    this.unsubscriber = null
+    this.state = {
+      isAuthenticated: false,
+      typedEmail: '',
+      typedPassword: '',
+      user:null
+    }
+  }
+//   componentDidMount(){
+//     this.unsubscriber = firebase.auth().onAuthStateChanged((changeUser)=>{
+//       console.log(`changed user : ${JSON.stringify(changedUser.toJSON())}`)
+//     })
+//   }
+// componentWillUnmount(){
+//   this.unsubscriber = firebase.auth()
+// }
+
+onLoginFacebook = () => {
+  LoginManager
+    .logInWithReadPermissions(['public_profile', 'email'])
+    .then((result) => {
+      // console.log(`Login Success with permission: ${result.grantedPermissions.toString()}`)
+      return AccessToken.getCurrentAccessToken();
+
+    })
+    .then(data => {
+      const credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken)
+      return firebase.auth().signInWithCredential(credential)
+    })
+    .then((currentUser)=>{
+      // console.log(`Facebook Login with user: ${JSON.stringify(currentUser.toJSON())}`)
+    })
+    .catch((error)=>{
+      // console.log(`Facebook Login Failed ${error}`)
+    })
+}
+
   render() {
     return (
       <View style={{flex: 1, marginTop: Platform.OS ==='ios' ? 34 :0}}>
         <Text style={{color: 'red'}}>Test React Native FireBase</Text>
+        <Button containerStyle={{
+          padding: 10,
+          width:150,
+          margin:20,
+          borderRadius: 4,
+          backgroundColor: 'rgb(73,104,173)'
+        }}
+        style={{fontSize: 18,color:'white'}}
+        onPress={this.onLoginFacebook}>Login Facebook</Button>
         </View>
     );
   }
